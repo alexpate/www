2a32407b9e9e874c6bdfd13db675716943c3fe4a
Alex Pate <alex@alexpate.uk>
2016-12-19 20:22:40 +0000

    Add AMP jekyll plugin

---
+++ _plugins/amp_generate.rb
@@ -0,0 +1,31 @@
+module Jekyll
+  # Defines the base class of AMP posts
+  class AmpPost < Page
+    def initialize(site, base, dir, post)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'amp.html')
+      self.content               = post.content
+      self.data['body']          = post.content
+      self.data['title']         = post.data['title']
+      self.data['date']          = post.data['date']
+      self.data['author']        = post.data['author']
+      self.data['category']      = post.data['category']
+      self.data['canonical_url'] = post.url
+    end
+  end
+  # Generates a new AMP post for each existing post
+  class AmpGenerator < Generator
+    priority :low
+    def generate(site)
+      dir = site.config['ampdir'] || 'amp'
+      site.posts.docs.each do |post|
+        next if post.data['skip_amp'] == true
+        site.pages << AmpPost.new(site, site.source, File.join(dir, post.id), post)
+      end
+    end
+  end
+end
